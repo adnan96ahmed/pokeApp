1701dca53c9e41d1ee813a53460f5e56d1477022
Adnan Ahmed <adnan_a96@hotmail.com>
2022-09-26 18:46:12 -0400

    updated settings

--- pokeApp/settings.py
+++ pokeApp/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+DISABLE_COLLECTSTATIC=1
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
